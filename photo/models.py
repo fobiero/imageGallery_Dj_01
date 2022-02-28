bd68f256b99d9bc692efc6ac61d832b398a7fadf
feLycks <felix.obiero@student.moringaschool.com>
2022-02-28 22:50:30 +0300

    models build

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,29 @@
+
 from django.db import models
 
 # Create your models here.
+
+from . import views
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Images(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+
+class Location(models.Model):
+    image = models.ForeignKey(Images, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.image
+
+
