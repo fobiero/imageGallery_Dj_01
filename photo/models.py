41f60d9ef86f089b8c530861664703d2127689d6
feLycks <felix.obiero@student.moringaschool.com>
2022-03-01 22:51:05 +0300

    make migrations

--- photo/models.py
+++ photo/models.py
@@ -1,9 +1,8 @@
 
 from django.db import models
+# from . import views
 
 # Create your models here.
-
-from . import views
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
