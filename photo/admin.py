9aa31249a3fc0e8ee7e121d09f99f36d26213746
feLycks <felix.obiero@student.moringaschool.com>
2022-03-01 23:32:03 +0300

    Logic build

--- photo/admin.py
+++ photo/admin.py
@@ -1,3 +1,10 @@
+from email.mime import image
+from unicodedata import category
 from django.contrib import admin
+from .models import Category, Images, Location
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Images)
+admin.site.register(Location)
--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Images, Location
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'photos/home.html')
+    categories = Category.objects.all()
+    all_cat = {'categories': categories}
+
+    return render(request, 'photos/home.html', all_cat)
 
 def viewPhoto(request, pk):
     return render(request, 'photos/photo.html')
