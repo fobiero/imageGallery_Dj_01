4d8e9ce2b325fa62f39da95b1363ba58a29df752
feLycks <felix.obiero@student.moringaschool.com>
2022-03-02 15:53:21 +0300

    edit app-settings

--- photosite/settings.py
+++ photosite/settings.py
@@ -179,10 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+
